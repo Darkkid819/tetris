34818b9cd2c99ae8879025219c0aac9b9b088022
Jordan Mitacek <69481257+Darkkid819@users.noreply.github.com>
2023-12-13 09:57:17 -0500

    Delete src/main directory

--- src/main/java/com/tetris/service/_GameStateBroadcaster.java
+++
@@ -1,7 +0,0 @@
-package com.tetris.service;
-
-import com.tetris.model.GameState;
-
-public interface _GameStateBroadcaster {
-    void broadcastState(GameState gameState);
-}
--- src/main/java/com/tetris/service/_PlayerService.java
+++
@@ -1,7 +0,0 @@
-package com.tetris.service;
-
-import com.tetris.model.Player;
-
-public interface _PlayerService {
-    Player createPlayer(String displayName);
-}
